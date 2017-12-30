0e4ec53d91e2159595b9c329c44ba518e510472c
Shikhar1998 <shikharmakhija2@gmail.com>
2017-12-30 16:57:34 +0530

    GCAF (#1)
    * Add files via upload
    
    GACF (Genetic Algorithm Caller Function)
    
    * Update main.m

---
+++ main.m
@@ -0,0 +1,6 @@
+%% Optimisation Tool Box Caller Function        %%
+% For optimisation, select the genetic algorithm %
+% option and set it's parameters                 % 
+function y = main(x)
+    y = BackPropAlgo(x);
+end
